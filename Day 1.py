00f3a2aedd802b213b7079b015358946de57d417
manii3m <manidemo33@gmail.com>
2025-11-27 00:45:29 +0530

    Create Day 1.py

---
+++ Day 1.py
@@ -0,0 +1,10 @@
+print (" Hello, World! ")
+name = " mayank "
+age = 20
+height = 5.6
+is_student = True
+print ("Name : ", name)
+print ("Age : ", age)
+print ("Height : ", height)
+print ("Is Student : ", is_student)
+print = input( " Enter your name here : " )
