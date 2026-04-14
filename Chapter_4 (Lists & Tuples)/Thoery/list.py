5774e1bfb2800252ff64f414041b0df67d0fe508
manii3m <manidemo33@gmail.com>
2026-04-14 23:07:20 +0530

    Started to learn concept of list

---
+++ Chapter_4 (Lists & Tuples)/Thoery/list.py
@@ -0,0 +1,9 @@
+friends = ['Apple', 'Orange', 5, 5.33, False, 'Akash']
+
+print(friends[1])
+
+friends[1] = 'Grapes'
+
+print(friends[1])
+print(friends[1:4])
+
---
+++ Chapter_4 (Lists & Tuples)/Thoery/list_methods.py
@@ -0,0 +1,18 @@
+friends = ['Apple', 'Orange', 5, 5.33, False, 'Akash']
+print(friends)
+
+friends.append('Mani')
+print(friends)
+
+sort = [1,3,4,2,44,12,8,6]
+sort.sort() # sort in ascending order
+print(sort)
+sort.reverse() # sort in descending order
+print(sort)
+sort = [1,3,4,2,44,12,8,6]
+sort.insert(3,42) #insert custom value at custom index (index, value)
+sort.sort() # sort in ascending order
+
+print(sort)
+sort.pop(3) # remove the n'th index value
+print(sort)
